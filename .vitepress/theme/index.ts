073be3c971ce966dda31353503bf1c9925b6315c
O K Z . . . <maratmatveev1337@gmail.com>
2025-07-10 16:52:21 +0300

    Added a analytics
    From vercel

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,17 +1,21 @@
-// https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
+// .vitepress/theme/index.ts
+import { h, onMounted } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+import { inject } from '@vercel/analytics'
 import './style.css'
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
+    return h(DefaultTheme.Layout)
   },
-  enhanceApp({ app, router, siteData }) {
-    // ...
+  enhanceApp() {
+    // Обеспечиваем запуск только на клиенте
+    if (typeof window !== 'undefined') {
+      onMounted(() => {
+        inject()
+      })
+    }
   }
 } satisfies Theme
